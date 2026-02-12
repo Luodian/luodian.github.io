c7974e6f1d1b456c921613b4b3c2a519ef4fd7e7
Bo Li <drluodian@gmail.com>
2026-02-12 11:11:45 +0800

    chore: switch Chinese font to Noto Sans SC

--- next-site/app/layout.tsx
+++ next-site/app/layout.tsx
@@ -1,24 +1,17 @@
 import type { ReactNode } from "react";
 import "./globals.css";
-import localFont from "next/font/local";
-import { GeistMono } from "geist/font/mono";
-import { GeistSans } from "geist/font/sans";
-import { GeistPixelSquare } from "geist/font/pixel";
-
-const ArkPixelZhCn = localFont({
-  src: "./fonts/ark-pixel-16px-proportional-zh_cn.otf.woff2",
-  variable: "--font-ark-pixel-zh-cn",
-  display: "swap",
-});
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={`${GeistSans.variable} ${GeistMono.variable} ${GeistPixelSquare.variable} ${ArkPixelZhCn.variable}`}
-    >
+    <html lang="en">
       <head>
         <link rel="preload" as="image" href="/vibe-bg.jpg" fetchPriority="high" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>{children}</body>
     </html>
